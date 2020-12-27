2920754128e0a0a7902de9d0895a95caadae664d
cedricve <cedric@verstraeten.io>
2020-12-27 21:46:27 +0100

    add goland configuration

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,10 @@
 module github.com/kerberos-io/opensource/backend
 
 go 1.15
+
+require (
+	github.com/appleboy/gin-jwt/v2 v2.6.4
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-contrib/pprof v1.3.0
+	github.com/gin-gonic/gin v1.6.3
+)
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,145 @@
 package main
 
 import (
-  "fmt"
-  "os"
+	"os"
+	"fmt"
+	"log"
+	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-contrib/pprof"
+	"github.com/kerberos-io/opensource/backend/src/models"
 )
 
 func main(){
-  fmt.Println(os.Getenv("HELLO_WORLD"))
-  fmt.Println("So is the first line of Kerberos Open Source v3")
-  return
+
+	const VERSION = 3.0
+	action := os.Args[1]
+
+	switch action {
+
+		case "version":
+
+			log.Printf("%s: %0.1f\n", "You are currrently running Kerberos Open Source", VERSION)
+
+		case "pending-upload":
+
+			name := os.Args[2]
+      		fmt.Println(name)
+			//cloud.PendingUpload(log, name)
+
+		case "discover":
+
+			timeout := os.Args[2]
+      		fmt.Println(timeout)
+			//duration, _ := strconv.Atoi(timeout)
+			//capture.Discover(log, time.Duration(duration))
+
+		case "run": {
+
+			name := os.Args[2]
+			port := os.Args[3]
+			fmt.Println(name)
+
+			// Initialize REST API
+			r := gin.Default()
+
+			// Profile
+  			pprof.Register(r)
+
+			// Setup CORS
+			r.Use(cors.New(cors.Config{
+				AllowOrigins:     []string{"*"},
+				AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+				AllowHeaders:     []string{"Origin", "Content-Type", "authorization", "multipart/form-data", "x-requested-with"},
+				ExposeHeaders:    []string{"Content-Length"},
+				AllowCredentials: true,
+				MaxAge: 12 * time.Hour,
+			}))
+
+			// the jwt middleware
+		  	identityKey := "id"
+		  	myKey := "TOBECHANGED"
+			authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+				Realm:       "kerberosio",
+				Key:         []byte(myKey),
+				Timeout:     time.Hour * 24,
+				MaxRefresh:  time.Hour * 24 * 7,
+				IdentityKey: identityKey,
+				PayloadFunc: func(data interface{}) jwt.MapClaims {
+					if v, ok := data.(*models.User); ok {
+						return jwt.MapClaims{
+							identityKey: v,
+						}
+					}
+					return jwt.MapClaims{}
+				},
+				IdentityHandler: func(c *gin.Context) interface{} {
+					claims := jwt.ExtractClaims(c)
+					user := claims["id"].(map[string]interface {})
+					return &models.User{
+						Username: user["username"].(string),
+						Role: user["role"].(string),
+					}
+				},
+				Authorizator: func(data interface{}, c *gin.Context) bool {
+					if _, ok := data.(*models.User); ok  { //&& v.Username == "admin" {
+						return true
+					}
+					return false
+				},
+				Unauthorized: func(c *gin.Context, code int, message string) {
+					c.AbortWithStatusJSON(code, gin.H{
+						"code":    code,
+						"message": message,
+					})
+				},
+				// TokenLookup is a string in the form of "<source>:<name>" that is used
+				// to extract token from the request.
+				// Optional. Default value "header:Authorization".
+				// Possible values:
+				// - "header:<name>"
+				// - "query:<name>"
+				// - "cookie:<name>"
+				// - "param:<name>"
+				TokenLookup: "header: Authorization, query: token, cookie: jwt",
+				// TokenLookup: "query:token",
+				// TokenLookup: "cookie:token",
+
+				// TokenHeadName is a string in the header. Default value is "Bearer"
+				TokenHeadName: "Bearer",
+
+				// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
+				TimeFunc: time.Now,
+			})
+
+			if err != nil {
+				log.Fatal("JWT Error:" + err.Error())
+			}
+
+			r.GET("/restart", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"restart": true,
+				})
+			})
+
+			r.GET("/motion", func(c *gin.Context) {
+				c.JSON(200, gin.H{
+					"data": "☄ Simulate motion",
+				})
+			})
+
+			r.Use(authMiddleware.MiddlewareFunc())
+			{
+
+			}
+
+			r.Run(":" + port)
+
+		}
+		default:
+			fmt.Println("Sorry I don't understand :(")
+	}
+	return
 }
