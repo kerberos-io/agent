1489016038004e223bbe4b0df096f4b5d6114d25
cedricve <cedric@verstraeten.io>
2020-12-29 17:10:07 +0100

    Revert "Delete App.test.js"
    This reverts commit 1812f1f6cb1ad6c2d2f09cfb2fc54e6a2b9af17e.

---
+++ web/src/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+// NO TESTS FOR NOW :(
+
+/*test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});*/
