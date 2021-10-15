8364df32f4f62eecc60be47e11dcc5cceb01a0df
Andrea Pavone <info@andreapavone.com>
2021-10-15 21:10:45 +0200

    fix yarn test failed issue #28

--- web/src/App.test.jsx
+++ web/src/App.test.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen, test, expect } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 /* Test default values by rendering a context consumer without a
