f2b1f82f593b8a598abc74efe34cf66e438c45a7
Thomas Quandalle <95466836+thomasqp@users.noreply.github.com>
2022-08-30 20:40:52 +0200

    Update Dashboard.jsx

--- ui/src/pages/Dashboard/Dashboard.jsx
+++ ui/src/pages/Dashboard/Dashboard.jsx
@@ -61,10 +61,10 @@ class Dashboard extends React.Component {
 
     // We check if a connection is made to Kerberos Hub, or if Offline mode
     // has been turned on.
-    //let offlineMode = false;
+    // let offlineMode = false;
     let cloudConnection = 'Not connected';
     if (dashboard.offlineMode === 'true') {
-      //offlineMode = true;
+      // offlineMode = true;
       cloudConnection = 'Offline mode';
     } else {
       // TODO check if cloud connection is working
