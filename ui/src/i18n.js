6cb790607615d546dcf5fd98a835916b48e4ce71
Cedric Verstraeten <cedric@verstraeten.io>
2022-09-04 21:11:51 +0200

    move translations not able to be resolved in container

--- ui/src/i18n.js
+++ ui/src/i18n.js
@@ -2,29 +2,12 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import translationEN from './locales/en/translation.json';
-import translationNL from './locales/nl/translation.json';
-import translationFR from './locales/fr/translation.json';
-
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  nl: {
-    translation: translationNL,
-  },
-  fr: {
-    translation: translationFR,
-  },
-};
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
-    // lng: 'en',
     fallbackLng: 'en',
     debug: false,
     interpolation: {
