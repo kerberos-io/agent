89c39aa85343f8dd8cb6d010f8476e85b6146b2e
Cédric Verstraeten <cedric@verstraeten.io>
2022-11-17 21:19:25 +0000

    make some modifications to show what needs to be uncommented for codespaces

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,10 +1,18 @@
 const { hostname, host, protocol } = window.location;
 
+// Uncomment this when using codespaces or other special DNS names (which you can't control)
+// replace this with the DNS name of the kerberos agent server (the codespace url)
+//const externalHost = 'xxx-8080.preview.app.github.dev';
+
 const dev = {
   ENV: 'dev',
   HOSTNAME: hostname,
   API_URL: `${protocol}//${hostname}:8080/api`,
   URL: `${protocol}//${hostname}:8080`,
+
+  // Uncomment, and comment the above lines, when using codespaces or other special DNS names (which you can't control)
+  //API_URL: `${protocol}//${externalHost}/api`,
+  //URL: `${protocol}//${externalHost}`,
 };
 
 const prod = {
