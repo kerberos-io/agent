21d81b94dd75cb201393a6482df775f26059896c
Cedric Verstraeten <cedric@verstraeten.io>
2024-01-16 09:47:07 +0100

    [release] v3.1.6

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -100,7 +100,7 @@ class App extends React.Component {
           </div>
         )}
         <div id="page-root">
-          <Sidebar logo={logo} title="Kerberos Agent" version="v3.1.5" mobile>
+          <Sidebar logo={logo} title="Kerberos Agent" version="v3.1.6" mobile>
             <Profilebar
               username={username}
               email="support@kerberos.io"
