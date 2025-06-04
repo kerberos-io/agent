919f21b48b5dd6711bdbfbef3a8ee88156498811
Cédric Verstraeten <cedric@verstraeten.io>
2025-06-04 08:45:54 +0000

    Refactor AddSampleToTrack method to create separate video and audio fragments, enhancing sample handling and improving error logging for AAC frames

--- machinery/src/video/mp4.go
+++ machinery/src/video/mp4.go
@@ -6,6 +6,7 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -150,12 +151,22 @@ func (mp4 *MP4) AddSampleToTrack(trackID uint32, isKeyframe bool, data []byte, p
 		seg := mp4ff.NewMediaSegment()
 
 		// Create a video fragment
-		videoFragment, err := mp4ff.CreateMultiTrackFragment(uint32(mp4.SegmentCount), []uint32{1, 2}) // Assuming 1 for video track and 2 for audio track
+		//videoFragment, err := mp4ff.CreateMultiTrackFragment(uint32(mp4.SegmentCount), []uint32{1, 2}) // Assuming 1 for video track and 2 for audio track
+
+		videoFragment, err := mp4ff.CreateFragment(uint32(mp4.SegmentCount), 1)
 		if err != nil {
 			return err
 		}
 		seg.AddFragment(videoFragment)
 		mp4.VideoFragment = videoFragment
+
+		// Create an audio fragment
+		audioFragment, err := mp4ff.CreateFragment(uint32(mp4.SegmentCount), 2)
+		if err != nil {
+			return err
+		}
+		seg.AddFragment(audioFragment)
+		mp4.AudioFragment = audioFragment
 
 		// Set to MP4 struct
 		mp4.Segment = seg
@@ -203,16 +214,20 @@ func (mp4 *MP4) AddSampleToTrack(trackID uint32, isKeyframe bool, data []byte, p
 		} else if trackID == uint32(mp4.AudioTrack) {
 
 			if mp4.AudioFullSample != nil {
-				duration := pts - mp4.AudioFullSample.DecodeTime
-				fmt.Printf("Adding sample to track %d, PTS: %d, Duration: %d, size: %d\n", trackID, pts, duration, len(data))
-				mp4.AudioTotalDuration += duration
-				mp4.AudioFullSample.DecodeTime = mp4.AudioTotalDuration - duration
-				mp4.AudioFullSample.Sample.Dur = uint32(duration)
-				err := mp4.VideoFragment.AddFullSampleToTrack(*mp4.AudioFullSample, trackID)
-				if err != nil {
-					log.Printf("Error adding sample to track %d: %v", trackID, err)
-					return err
-				}
+
+				SplitAACFrame(mp4.AudioFullSample.Data, func(aac []byte) {
+					duration := pts - mp4.AudioFullSample.DecodeTime
+					fmt.Printf("Adding sample to track %d, PTS: %d, Duration: %d, size: %d\n", trackID, pts, duration, len(aac[7:]))
+					mp4.AudioTotalDuration += duration
+					mp4.AudioFullSample.Data = aac[7:] // Remove the ADTS header (first 7 bytes)
+					mp4.AudioFullSample.DecodeTime = mp4.AudioTotalDuration - duration
+					mp4.AudioFullSample.Sample.Dur = uint32(duration)
+					mp4.AudioFullSample.Sample.Size = uint32(len(aac[7:]))
+					err := mp4.AudioFragment.AddFullSampleToTrack(*mp4.AudioFullSample, trackID)
+					if err != nil {
+						log.Printf("Error adding sample to track %d: %v", trackID, err)
+					}
+				})
 			}
 
 			// Set the sample data
@@ -307,7 +322,7 @@ func (mp4 *MP4) Close(config *models.Config, trackID uint32, pts uint64) {
 		// Add an audio track to the moov box
 		init.AddEmptyTrack(audioTimescale, "audio", "und")
 		// Set the audio descriptor
-		err = init.Moov.Traks[1].SetAACDescriptor(5, 48000)
+		err = init.Moov.Traks[1].SetAACDescriptor(2, 48000)
 		if err != nil {
 			//panic(err)
 		}
@@ -586,4 +601,335 @@ func findStartCode(data []byte, pos int) int {
 		}
 	}
 	return -1
+}
+
+// FindSyncword searches for the AAC syncword (0xFFF0) in the given byte slice starting from the specified offset.
+func FindSyncword(aac []byte, offset int) int {
+	for i := offset; i < len(aac)-1; i++ {
+		if aac[i] == 0xFF && aac[i+1]&0xF0 == 0xF0 {
+			return i
+		}
+	}
+	return -1
+}
+
+// Table 31 – Profiles
+// index      profile
+//   0        Main profile
+//   1        Low Complexity profile (LC)
+//   2        Scalable Sampling Rate profile (SSR)
+//   3        (reserved)
+
+type START_CODE_TYPE int
+
+const (
+	START_CODE_3 START_CODE_TYPE = 3
+	START_CODE_4 START_CODE_TYPE = 4
+)
+
+func FindStartCode(nalu []byte, offset int) (int, START_CODE_TYPE) {
+	idx := bytes.Index(nalu[offset:], []byte{0x00, 0x00, 0x01})
+	switch {
+	case idx > 0:
+		if nalu[offset+idx-1] == 0x00 {
+			return offset + idx - 1, START_CODE_4
+		}
+		fallthrough
+	case idx == 0:
+		return offset + idx, START_CODE_3
+	}
+	return -1, START_CODE_3
+}
+
+func SplitFrame(frames []byte, onFrame func(nalu []byte) bool) {
+	beg, sc := FindStartCode(frames, 0)
+	for beg >= 0 {
+		end, sc2 := FindStartCode(frames, beg+int(sc))
+		if end == -1 {
+			if onFrame != nil {
+				onFrame(frames[beg+int(sc):])
+			}
+			break
+		}
+		if onFrame != nil && onFrame(frames[beg+int(sc):end]) == false {
+			break
+		}
+		beg = end
+		sc = sc2
+	}
+}
+
+func SplitFrameWithStartCode(frames []byte, onFrame func(nalu []byte) bool) {
+	beg, sc := FindStartCode(frames, 0)
+	for beg >= 0 {
+		end, sc2 := FindStartCode(frames, beg+int(sc))
+		if end == -1 {
+			if onFrame != nil && (beg+int(sc)) < len(frames) {
+				onFrame(frames[beg:])
+			}
+			break
+		}
+		if onFrame != nil && (beg+int(sc)) < end && onFrame(frames[beg:end]) == false {
+			break
+		}
+		beg = end
+		sc = sc2
+	}
+}
+
+func SplitAACFrame(frames []byte, onFrame func(aac []byte)) {
+	var adts ADTS_Frame_Header
+	start := FindSyncword(frames, 0)
+	for start >= 0 {
+		adts.Decode(frames[start:])
+		onFrame(frames[start : start+int(adts.Variable_Header.Frame_length)])
+		start = FindSyncword(frames, start+int(adts.Variable_Header.Frame_length))
+	}
+}
+
+type AAC_PROFILE int
+
+const (
+	MAIN AAC_PROFILE = iota
+	LC
+	SSR
+)
+
+type AAC_SAMPLING_FREQUENCY int
+
+const (
+	AAC_SAMPLE_96000 AAC_SAMPLING_FREQUENCY = iota
+	AAC_SAMPLE_88200
+	AAC_SAMPLE_64000
+	AAC_SAMPLE_48000
+	AAC_SAMPLE_44100
+	AAC_SAMPLE_32000
+	AAC_SAMPLE_24000
+	AAC_SAMPLE_22050
+	AAC_SAMPLE_16000
+	AAC_SAMPLE_12000
+	AAC_SAMPLE_11025
+	AAC_SAMPLE_8000
+	AAC_SAMPLE_7350
+)
+
+var AAC_Sampling_Idx [13]int = [13]int{96000, 88200, 64000, 48000, 44100, 32000, 24000, 22050, 16000, 12000, 11025, 8000, 7350}
+
+// Table 4 – Syntax of adts_sequence()
+// adts_sequence() {
+//         while (nextbits() == syncword) {
+//             adts_frame();
+//         }
+// }
+// Table 5 – Syntax of adts_frame()
+// adts_frame() {
+//     adts_fixed_header();
+//     adts_variable_header();
+//     if (number_of_raw_data_blocks_in_frame == 0) {
+//         adts_error_check();
+//         raw_data_block();
+//     }
+//     else {
+//         adts_header_error_check();
+//         for (i = 0; i <= number_of_raw_data_blocks_in_frame;i++ {
+//             raw_data_block();
+//             adts_raw_data_block_error_check();
+//         }
+//     }
+// }
+
+// adts_fixed_header()
+// {
+//         syncword;                         12           bslbf
+//         ID;                                1            bslbf
+//         layer;                          2            uimsbf
+//         protection_absent;              1            bslbf
+//         profile;                        2            uimsbf
+//         sampling_frequency_index;       4            uimsbf
+//         private_bit;                    1            bslbf
+//         channel_configuration;          3            uimsbf
+//         original/copy;                  1            bslbf
+//         home;                           1            bslbf
+// }
+
+type ADTS_Fix_Header struct {
+	ID                       uint8
+	Layer                    uint8
+	Protection_absent        uint8
+	Profile                  uint8
+	Sampling_frequency_index uint8
+	Private_bit              uint8
+	Channel_configuration    uint8
+	Originalorcopy           uint8
+	Home                     uint8
+}
+
+// adts_variable_header() {
+//      copyright_identification_bit;               1      bslbf
+//      copyright_identification_start;             1      bslbf
+//      frame_length;                               13     bslbf
+//      adts_buffer_fullness;                       11     bslbf
+//      number_of_raw_data_blocks_in_frame;         2      uimsfb
+// }
+
+type ADTS_Variable_Header struct {
+	Copyright_identification_bit       uint8
+	copyright_identification_start     uint8
+	Frame_length                       uint16
+	Adts_buffer_fullness               uint16
+	Number_of_raw_data_blocks_in_frame uint8
+}
+
+type ADTS_Frame_Header struct {
+	Fix_Header      ADTS_Fix_Header
+	Variable_Header ADTS_Variable_Header
+}
+
+func NewAdtsFrameHeader() *ADTS_Frame_Header {
+	return &ADTS_Frame_Header{
+		Fix_Header: ADTS_Fix_Header{
+			ID:                       0,
+			Layer:                    0,
+			Protection_absent:        1,
+			Profile:                  uint8(MAIN),
+			Sampling_frequency_index: uint8(AAC_SAMPLE_44100),
+			Private_bit:              0,
+			Channel_configuration:    0,
+			Originalorcopy:           0,
+			Home:                     0,
+		},
+
+		Variable_Header: ADTS_Variable_Header{
+			copyright_identification_start:     0,
+			Copyright_identification_bit:       0,
+			Frame_length:                       0,
+			Adts_buffer_fullness:               0,
+			Number_of_raw_data_blocks_in_frame: 0,
+		},
+	}
+}
+
+func (frame *ADTS_Frame_Header) Decode(aac []byte) {
+	_ = aac[6]
+	frame.Fix_Header.ID = aac[1] >> 3
+	frame.Fix_Header.Layer = aac[1] >> 1 & 0x03
+	frame.Fix_Header.Protection_absent = aac[1] & 0x01
+	frame.Fix_Header.Profile = aac[2] >> 6 & 0x03
+	frame.Fix_Header.Sampling_frequency_index = aac[2] >> 2 & 0x0F
+	frame.Fix_Header.Private_bit = aac[2] >> 1 & 0x01
+	frame.Fix_Header.Channel_configuration = (aac[2] & 0x01 << 2) | (aac[3] >> 6)
+	frame.Fix_Header.Originalorcopy = aac[3] >> 5 & 0x01
+	frame.Fix_Header.Home = aac[3] >> 4 & 0x01
+	frame.Variable_Header.Copyright_identification_bit = aac[3] >> 3 & 0x01
+	frame.Variable_Header.copyright_identification_start = aac[3] >> 2 & 0x01
+	frame.Variable_Header.Frame_length = (uint16(aac[3]&0x03) << 11) | (uint16(aac[4]) << 3) | (uint16(aac[5]>>5) & 0x07)
+	frame.Variable_Header.Adts_buffer_fullness = (uint16(aac[5]&0x1F) << 6) | uint16(aac[6]>>2)
+	frame.Variable_Header.Number_of_raw_data_blocks_in_frame = aac[6] & 0x03
+}
+
+func (frame *ADTS_Frame_Header) Encode() []byte {
+	var hdr []byte
+	if frame.Fix_Header.Protection_absent == 1 {
+		hdr = make([]byte, 7)
+	} else {
+		hdr = make([]byte, 9)
+	}
+	hdr[0] = 0xFF
+	hdr[1] = 0xF0
+	hdr[1] = hdr[1] | (frame.Fix_Header.ID << 3) | (frame.Fix_Header.Layer << 1) | frame.Fix_Header.Protection_absent
+	hdr[2] = frame.Fix_Header.Profile<<6 | frame.Fix_Header.Sampling_frequency_index<<2 | frame.Fix_Header.Private_bit<<1 | frame.Fix_Header.Channel_configuration>>2
+	hdr[3] = frame.Fix_Header.Channel_configuration<<6 | frame.Fix_Header.Originalorcopy<<5 | frame.Fix_Header.Home<<4
+	hdr[3] = hdr[3] | frame.Variable_Header.copyright_identification_start<<3 | frame.Variable_Header.Copyright_identification_bit<<2 | byte(frame.Variable_Header.Frame_length<<11)
+	hdr[4] = byte(frame.Variable_Header.Frame_length >> 3)
+	hdr[5] = byte((frame.Variable_Header.Frame_length&0x07)<<5) | byte(frame.Variable_Header.Adts_buffer_fullness>>3)
+	hdr[6] = byte(frame.Variable_Header.Adts_buffer_fullness&0x3F<<2) | frame.Variable_Header.Number_of_raw_data_blocks_in_frame
+	return hdr
+}
+
+func SampleToAACSampleIndex(sampling int) int {
+	for i, v := range AAC_Sampling_Idx {
+		if v == sampling {
+			return i
+		}
+	}
+	panic("not Found AAC Sample Index")
+}
+
+func AACSampleIdxToSample(idx int) int {
+	return AAC_Sampling_Idx[idx]
+}
+
+// +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+// |  audio object type(5 bits)  |  sampling frequency index(4 bits) |   channel configuration(4 bits)  | GA framelength flag(1 bits) |  GA Depends on core coder(1 bits) | GA Extension Flag(1 bits) |
+// +--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+
+
+type AudioSpecificConfiguration struct {
+	Audio_object_type        uint8
+	Sample_freq_index        uint8
+	Channel_configuration    uint8
+	GA_framelength_flag      uint8
+	GA_depends_on_core_coder uint8
+	GA_extension_flag        uint8
+}
+
+func NewAudioSpecificConfiguration() *AudioSpecificConfiguration {
+	return &AudioSpecificConfiguration{
+		Audio_object_type:        0,
+		Sample_freq_index:        0,
+		Channel_configuration:    0,
+		GA_framelength_flag:      0,
+		GA_depends_on_core_coder: 0,
+		GA_extension_flag:        0,
+	}
+}
+
+func (asc *AudioSpecificConfiguration) Encode() []byte {
+	buf := make([]byte, 2)
+	buf[0] = (asc.Audio_object_type & 0x1f << 3) | (asc.Sample_freq_index & 0x0F >> 1)
+	buf[1] = (asc.Sample_freq_index & 0x0F << 7) | (asc.Channel_configuration & 0x0F << 3) | (asc.GA_framelength_flag & 0x01 << 2) | (asc.GA_depends_on_core_coder & 0x01 << 1) | (asc.GA_extension_flag & 0x01)
+	return buf
+}
+
+func (asc *AudioSpecificConfiguration) Decode(buf []byte) error {
+
+	if len(buf) < 2 {
+		return errors.New("len of buf < 2 ")
+	}
+
+	asc.Audio_object_type = buf[0] >> 3
+	asc.Sample_freq_index = (buf[0] & 0x07 << 1) | (buf[1] >> 7)
+	asc.Channel_configuration = buf[1] >> 3 & 0x0F
+	asc.GA_framelength_flag = buf[1] >> 2 & 0x01
+	asc.GA_depends_on_core_coder = buf[1] >> 1 & 0x01
+	asc.GA_extension_flag = buf[1] & 0x01
+	return nil
+}
+
+func ConvertADTSToASC(frame []byte) (*AudioSpecificConfiguration, error) {
+	if len(frame) < 7 {
+		return nil, errors.New("len of frame < 7")
+	}
+	adts := NewAdtsFrameHeader()
+	adts.Decode(frame)
+	asc := NewAudioSpecificConfiguration()
+	asc.Audio_object_type = adts.Fix_Header.Profile + 1
+	asc.Channel_configuration = adts.Fix_Header.Channel_configuration
+	asc.Sample_freq_index = adts.Fix_Header.Sampling_frequency_index
+	return asc, nil
+}
+
+func ConvertASCToADTS(asc []byte, aacbytes int) (*ADTS_Frame_Header, error) {
+	aac_asc := NewAudioSpecificConfiguration()
+	err := aac_asc.Decode(asc)
+	if err != nil {
+		return nil, err
+	}
+	aac_adts := NewAdtsFrameHeader()
+	aac_adts.Fix_Header.Profile = aac_asc.Audio_object_type - 1
+	aac_adts.Fix_Header.Channel_configuration = aac_asc.Channel_configuration
+	aac_adts.Fix_Header.Sampling_frequency_index = aac_asc.Sample_freq_index
+	aac_adts.Fix_Header.Protection_absent = 1
+	aac_adts.Variable_Header.Adts_buffer_fullness = 0x3F
+	aac_adts.Variable_Header.Frame_length = uint16(aacbytes)
+	return aac_adts, nil
 }
