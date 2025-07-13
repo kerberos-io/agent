f6046c6a6c4be19a11aea30ebc0eed543b459605
Cédric Verstraeten <cedric@verstraeten.io>
2025-07-13 20:48:45 +0200

    Update main.go

--- machinery/src/utils/main.go
+++ machinery/src/utils/main.go
@@ -23,7 +23,7 @@ import (
 	"github.com/kerberos-io/agent/machinery/src/models"
 )
 
-const VERSION = "3.3.5"
+const VERSION = "3.5.0"
 
 const letterBytes = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
